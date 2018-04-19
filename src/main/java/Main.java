61b33a63c03c0548d09dc425002c54bafce0cfef
Brady Pascoe <bpas247@hotmail.com>
2018-04-18 17:10:03 -0700

    Added example unit test for JUnit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    print("Hello World");
+  }
+
+  public static String print(String str) {
+    System.out.println(str);
+    return str;
   }
 }
