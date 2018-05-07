e43fc74dd586d119a7e4e8c8bc510f3b63e69297
Brady Pascoe <bpas247@hotmail.com>
2018-05-07 12:15:14 -0700

    Preliminary version for Storage object

---
+++ src/main/java/Storage.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.List;
+
+public class Storage extends HashSet<ItemMock> {
+
+    public List<ItemMock> search(ItemMock item) {
+        return null;
+    }
+
+    public ItemMock find(ItemMock item) {
+	return null;
+    }
+}
+
+class ItemMock {
+
+}
