ace8b25d4fd67b8d19d34bbbec49726f206162cf
Brady Pascoe <bpas247@hotmail.com>
2018-05-10 12:12:55 -0700

    Preliminary draft of Command

---
+++ src/main/java/Command.java
@@ -0,0 +1,21 @@
+public abstract class Command {
+  private String name; //Name of the Command
+
+  public Command(String n) {
+    name = n;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public abstract CommandStatus validate(String toValidate);
+
+  public abstract CommandStatus parse(String toParse);
+
+  public abstract CommandStatus run(String toParse, StorageMock storage);
+}
+
+class StorageMock { //For compilation
+
+}
