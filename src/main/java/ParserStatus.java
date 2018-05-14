1473831e5b354ded67a89ce205febdca16c1c99a
Brady Pascoe <bpas247@hotmail.com>
2018-05-14 12:42:05 -0700

    Added Javadoc for ParserStatus

--- src/main/java/ParserStatus.java
+++ src/main/java/ParserStatus.java
@@ -1,3 +1,12 @@
+/**
+ * This enum represents the state of a parsed command
+ * to be used to keep track of different errors that could
+ * exist within the parsed command String.
+ * 
+ * @author  Brady Pascoe
+ * @version 5/08/18
+ * @since   5/14/18
+ */
 public enum ParserStatus {
   OK,
   BAD_COMMAND,
