8e2e475ea418284f99bb4355511070bd661814bc
Brady Pascoe <bpas247@hotmail.com>
2018-05-20 19:00:58 -0700

    Rewrote main to call user interface

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    print("Hello World");
-  }
 
-  public static String print(String str) {
-    System.out.println(str);
-    return str;
+    new UserInterface().runProgram();
   }
 }
